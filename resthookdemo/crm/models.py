2d16465f4431fef103efc8231ff37dc012c99a12
Bryan Helmig <bryan@bryanhelmig.com>
2013-09-09 11:08:16 -0500

    randomly generate things on signup

--- resthookdemo/crm/models.py
+++ resthookdemo/crm/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+
 from tastypie.models import create_api_key
 
 
@@ -32,5 +33,34 @@ class Deal(models.Model):
 
     def __unicode__(self):
         return self.title
+
+
+def create_some_fixtures(sender, instance, created, **kwargs):
+    import names
+    import random
+
+    if not created:
+        return
+
+    for x in range(5):
+        first = names.get_first_name()
+        last = names.get_last_name()
+        full_name = u'{} {}'.format(first, last)
+        email = u'{}@example.com'.format(first.lower().strip())
+        Contact.objects.create(user=instance,
+                               full_name=full_name,
+                               email=email)
+
+        adj = random.choice(['crazy', 'black', 'fancy',
+                               'sneaky', 'giant', 'spicy'])
+        thing = random.choice(['shoe', 'cat', 'lamp',
+                               'car', 'parrot', 'burrito'])
+        title = u'{} wants a {} {}'.format(first, adj, thing)
+        description = 'This is not a very good description is it?'
+        Deal.objects.create(user=instance,
+                            title=title,
+                            description=description,
+                            value=random.random() * 100)
+models.signals.post_save.connect(create_some_fixtures, sender=User)
 
 models.signals.post_save.connect(create_api_key, sender=User)
